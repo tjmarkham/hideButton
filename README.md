8d702cef81c1d3db0f727463d26d9df451d45128
TJ Markham <tj@tjmarkham.com>
2024-01-31 20:05:41 -0800

    hideButton version 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # hideButton
-Hide the "Delete merged branches" button from GitLab
+Hide the "Delete merged branches" button from GitLab.
+
+## Release info
+All files and directories from this repo are included in the released extension except for:
+- /afphoto/
+- /test/
---
+++ scripts/hideButton.js
@@ -0,0 +1,13 @@
+'use strict';
+
+function hideButton() {
+  let buttonsToHide = document.querySelectorAll('[data-qa-selector="delete_merged_branches_button"]');
+
+  if (buttonsToHide.length > 0) {
+    buttonsToHide.forEach((buttonToHide) => {
+      buttonToHide.hidden = true;
+    });
+  }
+}
+
+hideButton();
